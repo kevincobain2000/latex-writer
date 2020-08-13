68636c3eb10b6741829c9689e1e4fcb351108703
Pulkit Kathuria <kevincobain2000@gmail.com>
2020-08-14 04:17:41 +0900

    init - latex writer

---
+++ src/js/app.js
@@ -0,0 +1,75 @@
+MathJax = {
+    tex: {
+      inlineMath: [['$', '$'],],
+    },
+};
+var editor;
+$(document).ready(function () {
+    editor = initEditor()
+    autoSave()
+});
+
+
+function initEditor() {
+    return new MediumEditor('.editable', {
+        extensions: {
+            // autolist: autolist, // init it after loading from localstorage to avoid range error
+            table: new MediumEditorTable(),
+            customHtmlP: new CustomHtml({
+                buttonText: "<p>",
+                htmlToInsert: "<p>"
+            }),
+            customHtmlHr: new CustomHtml({
+                buttonText: "<hr>",
+                htmlToInsert: "<hr>"
+            }),
+            customHtmlPre: new CustomHtml({
+                buttonText: "<pre>",
+                htmlToInsert: "<pre>"
+            }),
+            customHtmlSup: new CustomHtml({
+                buttonText: "<sup>",
+                htmlToInsert: "<sup>1</sup>"
+            }),
+            customHtmlSmall: new CustomHtml({
+                buttonText: "<small>",
+                htmlToInsert: "<small>small text</small>"
+            }),
+        },
+        buttonLabels: 'fontawesome',
+        toolbar: {
+            buttons: [
+                'customHtmlP',
+                'bold',
+                'italic',
+                'h1',
+                'h2',
+                'h3',
+                'quote',
+                'anchor',
+                'customHtmlPre',
+                'justifyCenter',
+                'justifyLeft',
+                'justifyRight',
+                'table',
+                'customHtmlHr',
+                'customHtmlSup',
+                'customHtmlSmall',
+                'orderedlist',
+                'unorderedlist',
+            ],
+            static: true,
+            sticky: true
+        },
+    });
+}
+
+function autoSave() {
+    if (typeof(Storage) !== "undefined") {
+        editor.setContent(window.localStorage.getItem('content'));
+        editor.subscribe('editableInput', function (eventObj, editable) {
+            window.localStorage.setItem('content', editor.getContent());
+        });
+        editor.extensions.autolist = new AutoList()
+    }
+}
---
+++ webpack.mix.js
@@ -0,0 +1,28 @@
+const mix = require('laravel-mix');
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.disableNotifications();
+mix.setPublicPath('./public/')
+
+mix
+  .sass('./src/css/style.scss', 'public/css/style.css')
+  .scripts([
+    './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/medium-editor/dist/js/medium-editor.min.js',
+    './node_modules/medium-editor-custom-html/src/custom-html.min.js',
+    './node_modules/medium-editor-tables/dist/js/medium-editor-tables.min.js',
+    './node_modules/medium-editor-autolist/dist/autolist.min.js',
+    './node_modules/mathjax/es5/tex-mml-chtml.js',
+    './src/js/app.js',
+  ], 'public/js/app.js')
+  .sourceMaps()
+  .version();
